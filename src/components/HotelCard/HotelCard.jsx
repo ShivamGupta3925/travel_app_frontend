72980078bc681484d74e98aa75a8c0b8a825f7a1
Shivam Gupta <shivamgupta3925@gmail.com>
2024-06-05 08:37:08 +0530

    feat : added navbar, category list and hotel listing page

---
+++ src/components/HotelCard/HotelCard.jsx
@@ -0,0 +1,33 @@
+import "./HotelCard.css"
+export const HotelCard = ({hotel}) => {
+
+  const { _id, name, image, address, state, rating, price } = hotel;
+  return (
+    <div className="relative hotelcard-container shadow cursor-pointer">
+      <div >
+        <img
+          className="img"
+          src={image}
+          alt={name}
+        />
+        <div className="hotelcard-details">
+          <span className="location">{address}, {state}</span>
+          <span className="rating d-flex align-center"></span>
+          <span class="material-icons-outlined">star</span>
+          <span>{rating}</span>
+        </div>
+        <p className="hotel-name">{name}</p>
+        <p className="price-details ">
+          <span className="price">Rs. {price}</span>
+          <span>night</span>
+        </p>
+      </div>
+      
+        <button className="button btn-wishlist absolute">
+          |<span class="material-icons favorite cursor">favorite </span>
+        </button>
+      
+    </div>
+  );
+};
+
